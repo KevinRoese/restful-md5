5d16526529bd4d6c9d7842dfb4eefaf8467c6d20
Kevin Roese <57219293+KevinRoese@users.noreply.github.com>
2022-05-08 16:00:06 +0200

    implemented example background thread

--- cracker.py
+++ cracker.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import json
+from random import random
 from time import sleep, time
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, render_template
 from flask_cors import CORS
 from flask_socketio import SocketIO, emit
+import threading
 
-async_mode = None
+async_mode = 'threading'
 
 app = Flask(__name__)
 #app.config['SECRET_KEY'] = 'thisisanexamplesecretkey'
 CORS(app)
 socketio = SocketIO(app, async_mode=async_mode)
+
+def task(start):
+    sum = 0
+    while (sum < 30):
+        sleep(1)
+        sum += random() + 0.5
+        print(sum)
+        if(sum >= 30):
+            runtime = time() - start
+            socketio.emit('result', {'time': runtime, 'sum': sum})
+            break
 
 @app.route('/')
 def index():
@@ -31,9 +43,11 @@ def hash(data):
         emit('error', {'message': 'No Hash received!'})
         return
     emit('info', {'message': '''Hash {} received!'''.format(rhash)})
+    print(rhash)
     global seconds
     seconds = time()
-    return
+    task_thread = threading.Thread(target=task, name='Task', args=(seconds,))
+    task_thread.start()
 
 @socketio.event
 def status():
@@ -41,11 +55,10 @@ def status():
     if(seconds == None):
         print('Status call before hash received!')
         emit('error', {'message': 'Status call before hash received!'})
+        return
     runtime = time() - seconds
     print('''Time: {}'''.format(runtime))
     emit('time', {'time': runtime})
-    if(runtime >= 30):
-        emit('result', {'time': runtime})
 
 @socketio.event
 def finished(data):
